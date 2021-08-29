0d3d80757c63a61c48f99888fb8d8acf95053764
sylphswind <87576170+sylphswind@users.noreply.github.com>
2021-08-29 11:51:50 -0500

    Add files via upload

---
+++ d.py
@@ -0,0 +1,42 @@
+
+import math
+
+print("Hello")
+
+index = ["A for adding", "D for Dividing", "M for Multiplying", "S for substracing", "For advanced type (random)"]
+
+print(index, ": Please enter the function you want capitalized")
+
+a = input("Enter function here: ")
+
+if a == "A":
+    val1 = int(input("Enter value of the first number: "))
+    val2 = int(input("Enter value of second number:" ))
+    print(val1 + val2)
+
+elif a == "M":
+    val1 = int(input("Enter value of the first number: "))
+    val2 = int(input("Enter value of second number:" ))
+    print(val1 * val2)
+
+elif a == "D":
+    val1 = int(input("Enter value of the first number: "))
+    val2 = int(input("Enter value of second number:" ))
+    print(val1/val2)
+
+elif a == "S":
+    val1 = int(input("Enter value of the first number: "))
+    val2 = int(input("Enter value of second number:" ))
+    print(val1 - val2)
+
+elif a == "random":
+    ad_index = ["Sq for square root", "E for exponentation"]
+    ad = input("Enter function: ")
+    if ad == "Sq":
+        val1 = int(input("Enter value of the number: "))
+        print(math.sqrt(val1))
+    elif ad == "E":
+        val1 = int(input("Enter value of the first number: "))
+        print(math.exp(val1))
+
+
